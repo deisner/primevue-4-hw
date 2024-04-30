5ce2776be7cba3a152c60f95b5f40845548ba526
David Eisner <deisner@gmail.com>
2024-04-30 17:02:33 +0000

    Add PrimeVue 4

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
+import PrimeVueStyled from 'primevue/styled'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVueStyled)
+app.mount('#app')
+// createApp(App).mount('#app')
